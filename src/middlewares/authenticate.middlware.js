8d5901ed35fa94cab8cd684b600314fabf42d3de
hamid2117 <saim008865@gmail.com>
2025-03-14 22:11:04 +0500

    feat: enhance token verification and JWT expiration handling

--- src/middlewares/authenticate.middlware.js
+++ src/middlewares/authenticate.middlware.js
@@ -1,21 +1,17 @@
-const jwt = require('jsonwebtoken')
-const User = require('../modules/user/user.model')
+const CustomError = require('../../errors')
+const { isTokenValid } = require('../utils')
 
-module.exports = async function verifyToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  if (!authHeader)
-    return res.status(401).json({ message: 'Missing authorization header' })
-  const token = authHeader.split(' ')[1]
-  if (!token) return res.status(401).json({ message: 'Token not provided' })
+module.exports = async function verifyToken(req, _res, next) {
+  const token = req.signedCookies.token
 
-  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Invalid token' })
-
-    req.user = {
-      id: decoded.id,
-      email: decoded.email,
-      permissions: decoded.permissions,
-    }
+  if (!token) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
+  }
+  try {
+    const { id, email, permissions } = isTokenValid({ token })
+    req.user = { id, email, permissions }
     next()
-  })
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
+  }
 }
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken')
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET)
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME || '24h',
+  })
   return token
 }
 
